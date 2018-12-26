3cc68c79988002c05246060b02c6bd19c92c3eba
Alberto <snk_snake@hotmail.com>
2018-12-26 23:36:33 +0100

    Personalizar una notificación FCM recibida en la bandeja del sistema

--- app/src/main/java/org/example/eventos/Comun.java
+++ app/src/main/java/org/example/eventos/Comun.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -67,5 +69,51 @@ public class Comun {
         tarea.contexto = context;
         tarea.idRegistroTarea = idRegistro;
         tarea.execute();
+    }
+
+    public static void eliminarIdRegistro(Context context) {
+        desregistrarDispositivoEnServidorWebTask tarea = new desregistrarDispositivoEnServidorWebTask();
+        tarea.contexto = context;
+        tarea.idRegistroTarea = FirebaseInstanceId.getInstance().getToken();
+        tarea.execute();
+    }
+
+    public static class desregistrarDispositivoEnServidorWebTask extends AsyncTask<Void, Void, String> {
+        String response = "error";
+        Context contexto;
+        String idRegistroTarea;
+
+        public void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(Void... arg0) {
+            try {
+                Uri.Builder constructorParametros = new Uri.Builder().appendQueryParameter("iddevice", idRegistroTarea).appendQueryParameter("idapp", ID_PROYECTO);
+                String parametros = constructorParametros.build().getEncodedQuery();
+                String url = URL_SERVIDOR + "desregistrar.php";
+                URL direccion = new URL(url);
+                HttpURLConnection conexion = (HttpURLConnection) direccion.openConnection();
+                conexion.setRequestMethod("POST");
+                conexion.setRequestProperty("Accept-Language", "UTF-8");
+                conexion.setDoOutput(true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(conexion.getOutputStream());
+                outputStreamWriter.write(parametros.toString());
+                outputStreamWriter.flush();
+                int respuesta = conexion.getResponseCode();
+                if (respuesta == 200) {
+                    response = "ok";
+                } else {
+                    response = "error";
+                }
+            } catch (IOException e) {
+                response = "error";
+            }
+            return response;
+        }
+
+        public void onPostExecute(String res) {
+        }
     }
 }
