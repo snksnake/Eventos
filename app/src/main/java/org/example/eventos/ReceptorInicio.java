60cc0438ad7422cd3818d2b9320ced2b092828ec
Alberto <snk_snake@hotmail.com>
2018-12-26 22:51:36 +0100

    BroadcastReceiver en aplicaciones FCM en Android

---
+++ app/src/main/java/org/example/eventos/ReceptorInicio.java
@@ -0,0 +1,12 @@
+package org.example.eventos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ReceptorInicio extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        context.startService(new Intent(context, EventosFCMService.class));
+    }
+}
