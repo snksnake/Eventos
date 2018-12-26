bb0c18ffc645e3a0c80ad42d2f59b0c10d6c0f2a
Alberto <snk_snake@hotmail.com>
2018-12-26 23:03:04 +0100

    Suscribir y dar de baja a usuarios en temas en FCM.

--- app/src/main/java/org/example/eventos/ActividadPrincipal.java
+++ app/src/main/java/org/example/eventos/ActividadPrincipal.java
@@ -1,5 +1,6 @@
 package org.example.eventos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,23 +80,18 @@ public class ActividadPrincipal extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_actividad_principal, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_temas) {
+            Intent intent = new Intent(getBaseContext(), Temas.class);
+            startActivity(intent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
